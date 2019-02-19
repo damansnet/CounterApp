a7f83b08f89f670a45de6367978e6e150c3aa4b7
Daman Pal singh <deepeesingh@gmail.com>
2019-02-19 15:04:04 +1100

    changed the architecture of the app

---
+++ Counter.Engine/Rules/CounterRules.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Counter.Engine.Rules
+{
+    public enum CounterRules
+    {
+        [Description("Find Average length of word starting with {0}")]
+        average_length_of_words_starting_with = 1,
+        [Description("Count of {1} in words starting with {0}")]
+        count_of_n_in_words_starting_with_x =2,
+        [Description("Find the longest word starting with {0}")]
+        longest_word_starting_with_abc = 3,
+        [Description("Count the sequence of words starting with {0} and {1}")]
+        count_of_sequence_of_words_starting_with_c_and_a=4
+    }
+
+    public  class RuleOutput
+    {
+        private  IDictionary<CounterRules, string> OutputHash = new Dictionary<CounterRules, string>() ;
+
+       public RuleOutput()
+        {
+            OutputHash.Add(CounterRules.average_length_of_words_starting_with, "average_length_of_words_starting_with_{0}.txt");
+            OutputHash.Add(CounterRules.count_of_n_in_words_starting_with_x, "count_of_{1}_in_words_starting_with_{0}.txt");
+            OutputHash.Add(CounterRules.count_of_sequence_of_words_starting_with_c_and_a, "count_of_sequence_of_words_starting_with_{0}_and_{1}.txt");
+            OutputHash.Add(CounterRules.longest_word_starting_with_abc, "longest_word_starting_with_{1}.txt");
+
+        }
+
+        public string GetOutputFileName(CounterRules rule)
+        {
+            return OutputHash[rule].ToString();
+        }
+        
+    }
+
+    
+}
--- CounterRules/Program.cs
+++ CounterRules/Program.cs
@@ -1,27 +1,29 @@
-﻿using System;
+﻿using Counter.Engine;
+using Counter.Engine.Rules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace CounterRules
+namespace CounterApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string regexStartWith = @"(?<!\w)[{0}]\w+";
+            //string regexStartWith = @"(?<!\w)[{0}]\w+";
             //count rule 1
             /*
             For all the words startw with 'a' or 'A', count average length of words, save the result. to file "average_length_of_words_starting_with_a.txt
              */
             string s = "Lorem ipsum text Second cold amp lorem ipsum. How are You. It's ok. Done. bell beet BEat Something else now. Cut Apple ";
-            var matches = Regex.Matches(s, string.Format(regexStartWith, "a|A"));// "(?<!\w)[a|A]\w+");
+            //var matches = Regex.Matches(s, string.Format(regexStartWith, "a|A"));// "(?<!\w)[a|A]\w+");
 
-            var totalLength = matches.Cast<Match>().Select(m => m.Length).Sum();
-            var average = totalLength / matches.Cast<Match>().Count();
-            Console.WriteLine(average);
+            //var totalLength = matches.Cast<Match>().Select(m => m.Length).Sum();
+            //var average = totalLength / matches.Cast<Match>().Count();
+            //Console.WriteLine(average);
           
 
             //count rule 2
@@ -29,11 +31,11 @@ namespace CounterRules
              for all the words starts with "b" or "B", count the total number of "e" or "E" in the words, save the result to the file "count_of_e_in_words_starting_with_b.txt
              */
            // string s = "Lorem ipsum text Second lorem ipsum. How are You. It's ok. Done. Something else now. Apple";
-            var matchesRule2 = Regex.Matches(s, string.Format(regexStartWith, "b|B"));
-            var findLetterRegEx = @"[e|E]";
-            var totalLength2 = matchesRule2.Cast<Match>().Select(m =>Regex.Matches(m.Value,findLetterRegEx).Count).Sum();
-            var count_of_e_in_words_starting_b = totalLength2;
-            Console.WriteLine(count_of_e_in_words_starting_b);
+            //var matchesRule2 = Regex.Matches(s, string.Format(regexStartWith, "b|B"));
+            //var findLetterRegEx = @"[e|E]";
+            //var totalLength2 = matchesRule2.Cast<Match>().Select(m =>Regex.Matches(m.Value,findLetterRegEx).Count).Sum();
+            //var count_of_e_in_words_starting_b = totalLength2;
+            //Console.WriteLine(count_of_e_in_words_starting_b);
             
 
 
@@ -42,36 +44,38 @@ namespace CounterRules
              For all the words starts with a or b or c get the longest length of words. save the result to the file.
              longest_words_starting with_abc.txt
              */
-            var matchesRule3= Regex.Matches(s, string.Format(regexStartWith, "a|A|b|B|c|C"));
-            var longestLength = matchesRule3.Cast<Match>().Select(m =>m.Length).Max();
-            Console.WriteLine(longestLength);
+            //var matchesRule3= Regex.Matches(s, string.Format(regexStartWith, "a|A|b|B|c|C"));
+            //var longestLength = matchesRule3.Cast<Match>().Select(m =>m.Length).Max();
+            //Console.WriteLine(longestLength);
 
             //count rule 4
-            var rule4 = @"(?<!\w)[{0}](\w+)\s((?<!\w)[{1}](\w+))";
-            var matchesRule4 = Regex.Matches(s, string.Format(rule4, "c|C","a|A"));
-            var matchesRule4_1 = string.Format(regexStartWith, "a|A");
-            var numberofOccurence = matchesRule4.Cast<Match>().Select(m => m.Groups[2].Value).Count();
-            Console.WriteLine(numberofOccurence);
+            //var rule4 = @"(?<!\w)[{0}](\w+)\s((?<!\w)[{1}](\w+))";
+            //var matchesRule4 = Regex.Matches(s, string.Format(rule4, "c|C","a|A"));
+            //var matchesRule4_1 = string.Format(regexStartWith, "a|A");
+            //var numberofOccurence = matchesRule4.Cast<Match>().Select(m => m.Groups[2].Value).Count();
+            //Console.WriteLine(numberofOccurence);
 
-            Start();
+            Start(s);
 
             Console.Read();
         }
 
-        public static void Start()
+        public static void Start(string s)
         {
 
-            CounterEngine.CounterEngine engine = new CounterEngine.CounterEngine();
-            Array AllRules = Enum.GetValues(enumType: typeof(CounterRules.Rules.CounterRules));
+            CounterEngine engine = new CounterEngine(s);
+            Array AllRules = Enum.GetValues(enumType: typeof(CounterRules));
+            RuleOutput ouptutFile = new RuleOutput();
            // string[] Rules = AllRules.Split('|');
-            List<string> listOfInputs = new List<string>();
+            
             Console.WriteLine("You can skip rule by entering -1 for input.");
             foreach (var rule in AllRules)
             {
 
+                List<string> listOfInputs = new List<string>();
                 System.Diagnostics.Debug.Print(rule.ToString());
 
-                CounterRules.Rules.CounterRules operation = (CounterRules.Rules.CounterRules) Enum.Parse(typeof(CounterRules.Rules.CounterRules),Convert.ToString(rule) );
+                var operation = (CounterRules) Enum.Parse(typeof(CounterRules),Convert.ToString(rule) );
                 var description = engine.GetRuleDescription(operation);
                 Console.WriteLine(description);
                 var pattern = @"{(.*?)}";
@@ -86,7 +90,11 @@ namespace CounterRules
                 }
                 if(listOfInputs.Count>0)
                 {
-                    engine.Process(operation, listOfInputs);
+                   var outputResult=  engine.Process(operation, listOfInputs);
+                    string fileName = System.Environment.CurrentDirectory.ToString();
+                    fileName = fileName + "\\"+ ouptutFile.GetOutputFileName(operation);
+
+                    OutputWriter.WriteToFile(outputResult, fileName);
                 }
 
             }
